a2ea75c36b3cd87a2a4f7efcf5a3efad4960e317
sivasomanath2502 <gsomanath2502@gmail.com>
2026-04-27 21:02:27 +0530

    FIles modified

--- online_judge/online-judge-backend/worker-service/src/main/java/com/onlinejudge/worker_service/repository/ProblemRepository.java
+++ online_judge/online-judge-backend/worker-service/src/main/java/com/onlinejudge/worker_service/repository/ProblemRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface ProblemRepository extends JpaRepository<Problem, UUID> {
+public interface ProblemRepository extends JpaRepository<Problem, String> {
 }
